b4c233540f5a3cdbb0e8ba75cce5284cfa7a17f2
RAMON PARRA <aldairparrag@gmail.com>
2019-04-07 17:54:47 -0500

    Imprimir pantalla principal y manejar touch para mostrar detalle de cada elemento

--- src/main.cpp
+++ src/main.cpp
@@ -2,37 +2,69 @@
 #include <SPI.h>
 #include "Adafruit_GFX.h"
 #include "Adafruit_ILI9341.h"
+#include <Adafruit_FT6206.h>
 
 #define LED 2
-
 //Pins de LCD
 #define TFT_DC 5
 #define TFT_CS 4
 #define TFT_RST 17
+//Pin interrupcion Touch
+#define TOUCH_INT 25
 
 //Declaración de funciones
+void evaluateChoseRect(int x, int y);
+void handleTouch();
 void initPins();
 void drawMainScreen();
 void tooglePin();
 void testCircles();
 unsigned long testFillScreen();
 
+//Coordenadas de cuadros
+int startRec1X, startRec1Y, endRec1X, endRec1Y;
+int startRec2X, startRec2Y, endRec2X, endRec2Y;
+int startRec3X, startRec3Y, endRec3X, endRec3Y;
+
+//Variables
+bool inMainScreen = true;
+unsigned long touchDebounceRef = 0;
+volatile bool touched = false;
+
 //Objetos
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
+Adafruit_FT6206 ts = Adafruit_FT6206();
+portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
+
+//Funcion para manejar interrupcion externa
+void IRAM_ATTR handleTouchInterrupt()
+{
+  touched = true;
+}
 
 void setup()
 {
+  //Inicializar comunicacion serial
+  Serial.begin(9600);
+
   //Inicializar pines
   initPins();
 
   //Inicializar LCD
   tft.begin();
+  tft.setRotation(1);
 
-  //Inicializar comunicacion serial
-  Serial.begin(9600);
+  //Inicializar touch
+  if (!ts.begin(40))
+  {
+    Serial.println("Error al iniciar touch");
+  }
+  else
+  {
+    Serial.println("Touch inicializado");
+  }
 
-  //Tests de pantalla
-  testFillScreen();
+  //Dibujar pantalla principal
   drawMainScreen();
 }
 
@@ -40,13 +72,19 @@ void loop()
 {
   //Toogle Pin
   tooglePin();
+
+  //Manejar touch
+  handleTouch();
 }
 
 /** Definición de Funciones **/
-
 void initPins()
 {
   pinMode(LED, OUTPUT);
+
+  //Pin de interrupcion touch
+  pinMode(TOUCH_INT, INPUT_PULLUP);
+  attachInterrupt(TOUCH_INT, handleTouchInterrupt, FALLING);
 }
 
 void tooglePin()
@@ -62,15 +100,14 @@ void tooglePin()
 
 void drawMainScreen()
 {
-  //Definir rotaion
-  tft.setRotation(1);
-
   //Mostrar dimensiones
   Serial.print("\n\n\n\n\n");
   Serial.print("Width -> ");
   Serial.println(tft.width());
   Serial.print("Height -> ");
   Serial.println(tft.height());
+
+  tft.fillScreen(ILI9341_BLACK);
 
   //Escribir título
   tft.setCursor((tft.width() / 3) - 8, 6);
@@ -79,28 +116,37 @@ void drawMainScreen()
   tft.println("Monitor GEA");
 
   //Dibujar Elementos
-  int centerRec1X = 6;
-  tft.fillRect(centerRec1X, 28, (tft.width() - 18) / 3, tft.height() - 36, ILI9341_BLUE);
-  tft.drawRect(centerRec1X, 28, (tft.width() - 18) / 3, tft.height() - 36, ILI9341_WHITE);
+  startRec1X = 6;
+  startRec1Y = 28;
+  endRec1X = startRec1X + (tft.width() - 18) / 3;
+  endRec1Y = startRec1Y + tft.height() - 36;
+  tft.fillRect(startRec1X, startRec1Y, (tft.width() - 18) / 3, tft.height() - 36, ILI9341_RED);
+  tft.drawRect(startRec1X, startRec1Y, (tft.width() - 18) / 3, tft.height() - 36, ILI9341_WHITE);
 
-  int centerRec2X = centerRec1X + (tft.width() - 18) / 3 + 6;
-  tft.fillRect(centerRec2X, 28, (tft.width() - 18) / 3, tft.height() - 36, ILI9341_BLUE);
-  tft.drawRect(centerRec2X, 28, (tft.width() - 18) / 3, tft.height() - 36, ILI9341_WHITE);
+  startRec2X = endRec1X + 6;
+  startRec2Y = 28;
+  endRec2X = startRec2X + (tft.width() - 18) / 3;
+  endRec2Y = startRec2Y + tft.height() - 36;
+  tft.fillRect(startRec2X, startRec1Y, (tft.width() - 18) / 3, tft.height() - 36, ILI9341_GREEN);
+  tft.drawRect(startRec2X, startRec1Y, (tft.width() - 18) / 3, tft.height() - 36, ILI9341_WHITE);
 
-  int centerRec3X = centerRec2X + (tft.width() - 18) / 3 + 6;
-  tft.fillRect(centerRec3X, 28, (tft.width() - 18) / 3, tft.height() - 36, ILI9341_BLUE);
-  tft.drawRect(centerRec3X, 28, (tft.width() - 18) / 3, tft.height() - 36, ILI9341_WHITE);
+  startRec3X = endRec2X + 6;
+  startRec3Y = 28;
+  endRec3X = startRec3X + (tft.width() - 18) / 3;
+  endRec3Y = startRec3Y + tft.height() - 36;
+  tft.fillRect(startRec3X, startRec3Y, (tft.width() - 18) / 3, tft.height() - 36, ILI9341_YELLOW);
+  tft.drawRect(startRec3X, startRec3Y, (tft.width() - 18) / 3, tft.height() - 36, ILI9341_WHITE);
 
   //Dibujar textos
   tft.setTextSize(1);
 
-  int centerText1X = centerRec1X + 28;
+  int centerText1X = startRec1X + 28;
   int centerText1Y = (tft.height() / 2) + 8;
 
-  int centerText2X = centerRec2X + 28;
+  int centerText2X = startRec2X + 28;
   int centerText2Y = centerText1Y;
 
-  int centerText3X = centerRec3X + 28;
+  int centerText3X = startRec3X + 28;
   int centerText3Y = centerText1Y;
 
   tft.setCursor(centerText1X + 8, centerText1Y);
@@ -109,8 +155,11 @@ void drawMainScreen()
   tft.setCursor(centerText2X + 8, centerText2Y);
   tft.println("Elec.");
 
+  tft.setTextColor(ILI9341_BLACK);
   tft.setCursor(centerText3X + 8, centerText3Y);
   tft.println("Agua");
+
+  inMainScreen = true;
 }
 
 unsigned long testFillScreen()
@@ -158,4 +207,74 @@ void testCircles()
     }
   }
   delay(5);
+}
+
+void handleTouch()
+{
+  if (touched && millis() - touchDebounceRef > 200)
+  {
+    //Obtener coordenada
+    TS_Point p = ts.getPoint();
+
+    if (p.x != 0 && p.x != 0)
+    {
+      int realY = tft.height() - p.x;
+      int realX = p.y;
+
+      //Mostrar datos de coordenada
+      Serial.print("x: ");
+      Serial.print(realX);
+      Serial.print("  y: ");
+      Serial.println(realY);
+      touchDebounceRef = millis();
+
+      if (inMainScreen)
+      {
+        evaluateChoseRect(realX, realY);
+      }
+      else
+      {
+        drawMainScreen();
+      }
+    }
+
+    touched = false;
+  }
+}
+
+void evaluateChoseRect(int x, int y)
+{
+  if (x >= startRec1X && x <= endRec1X &&
+      y >= startRec1Y && y <= endRec1Y)
+  {
+    tft.fillScreen(ILI9341_RED);
+    tft.setTextColor(ILI9341_WHITE);
+    tft.setTextSize(3);
+    tft.setCursor((tft.width() / 2) - 24, (tft.height() / 2) - 8);
+    tft.print("15%");
+
+    inMainScreen = false;
+  }
+  else if (x >= startRec2X && x <= endRec2X &&
+           y >= startRec2Y && y <= endRec2Y)
+  {
+    tft.fillScreen(ILI9341_GREEN);
+    tft.setTextColor(ILI9341_WHITE);
+    tft.setTextSize(3);
+    tft.setCursor(tft.width() / 3, (tft.height() / 2) - 8);
+    tft.print("1 kW/h");
+
+    inMainScreen = false;
+  }
+  else if (x >= startRec3X && x <= endRec3X &&
+           y >= startRec3Y && y <= endRec3Y)
+  {
+    tft.fillScreen(ILI9341_YELLOW);
+    tft.setTextColor(ILI9341_BLACK);
+    tft.setTextSize(3);
+    tft.setCursor(tft.width() / 3, (tft.height() / 2) - 8);
+    tft.print("3 L/h");
+
+    inMainScreen = false;
+  }
 }
